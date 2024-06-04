5076849c3fd518824404e15129653feef9032f4b
Tarang Lakhmapurkar <tarangjlakh@gmail.com>
2024-06-04 11:40:55 +0530

    adding Notes

---
+++ src/main/java/com/scaler/demo/entitites/NoteEntity.java
@@ -0,0 +1,10 @@
+package com.scaler.demo.entitites;
+
+import lombok.Data;
+
+@Data
+public class NoteEntity {
+	private int id;
+	private String title;
+	private String body;
+}
---
+++ src/main/java/com/scaler/demo/service/NoteService.java
@@ -0,0 +1,7 @@
+package com.scaler.demo.service;
+
+public class NoteService {
+	class TaskNoteHolder {
+		private int noteId = 1;
+	}
+}
